900b0835b4feff2e5f246c1894b611b6d855f557
payalpm <payalmanghnani11@gmail.com>
2023-03-13 17:12:33 +0530

    update

---
+++ B45JavaProject/src/day7/Executor.java
@@ -0,0 +1,15 @@
+package day7;
+
+public class Executor extends OverrideDemo {
+	
+	public void m1 (String e, int p) {
+		System.out.println("Seconddemo");
+	}
+	
+	public static void main(String[] args) {
+		Executor obj=new Executor();
+		obj.m1("sun",47);
+		
+		
+	}
+}
